c84dd3841a4723bda29dfdb4dfcfb8f3f9ca8b75
Scott Burns <scott.s.burns@gmail.com>
2011-08-15 12:31:27 -0500

    testing group_filter

--- pycap.py
+++ pycap.py
@@ -28,28 +28,43 @@ if ('KEYS' not in rc_data) or (study not in rc_data['KEYS']):
 
 project = rc.RCProject(rc_data['KEYS'][study], study)
 
+q1 = rc.Query('subjage', {'le':12, 'ge':8}, 'number')
+q2 = rc.Query('wjbrsss', {'ge':75},'number')
 
-query = rc.Query('subjage', [{'le':10}], 'number')
-good = project.single_filter(query)
+match1 = project.single_filter(q1)
+match2 = project.single_filter(q2)
+print("Manually made...")
+man = sorted(set(match1) & set(match2))
+print(man)
+print(len(man))
+
+query_group = rc.QueryGroup(q1)
+query_group.add_query(q2, 'AND')
+auto = project.group_filter(query_group)
+print("Made with QueryGroup")
+print(sorted(auto))
+print(len(auto))
 
 
-q1 = rc.Query('cat', [{'ge':12}])
-q2 = rc.Query('ind', [{'ge':12}])
-q3 = rc.Query('wjwa', [{'ge':12}])
-q4 = rc.Query('wjlid', [{'ge':12}])
 
-qg = rc.QueryGroup(q1)
-qg.add_query(q2)
-qg.add_query(q3)
-qg.add_query(q4)
+# q1 = rc.Query('score', {'ge':12})
+# q2 = rc.Query('index', {'ge':75})
+# q3 = rc.Query('cat', {'eq':'RD'}, 'string')
+# 
+# qg = rc.QueryGroup(q1)
+# qg.add_query(q2)
+# qg.add_query(q3)
+# 
+# 
+# q5 = rc.Query('iq', {'ge':85})
+# q6 = rc.Query('subscore', {'ge':74})
+# 
+# qg2 = rc.QueryGroup(q5)
+# qg2.add_query(q6, 'OR')
+# 
+# qg.add_query(qg2, 'OR')
+# 
+# print qg
+# 
+# print qg.fields()
 
-
-q5 = rc.Query('cat', [{'ge':12}])
-q6 = rc.Query('ind', [{'ge':12}])
-
-qg2 = rc.QueryGroup(q5)
-qg2.add_query(q6, 'OR')
-
-qg.add_query(qg2, 'OR')
-
-print qg
