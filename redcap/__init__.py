3b9ead34193644e0d12b2a855bfc815b9fda6556
Scott Burns <scott.s.burns@gmail.com>
2011-09-27 13:15:40 -0500

    Add relative imports in __init__

--- redcap/__init__.py
+++ redcap/__init__.py
@@ -2,11 +2,19 @@
 # -*- coding: utf-8 -*-
 
 """
+This module exposes the REDCap API through the Project class. Instantiate the
+class with the URL to your REDCap system along with an API key, probably
+generated for you by a REDCap administrator.
+
+With a Project object, you can view the metadata and export/import data. You
+can also build search queries with the Query/QueryGroup classes and use those
+in conjunction with your Project to filter your data.
+
 Copyright (c) 2011, Scott Burns
 All rights reserved.
 """
 __version__ = '0.3'
 
-from project import Project
-from query import Query, QueryGroup
-from request import RCRequest, RCAPIError
+from .project import Project
+from .query import Query, QueryGroup
+from .request import RCRequest, RCAPIError
