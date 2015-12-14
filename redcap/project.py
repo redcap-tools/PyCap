a2775c2f89be3f8ce288b9250ed54b6faedb1a5d
Christopher Fonnesbeck <chris.fonnesbeck@vanderbilt.edu>
2015-12-14 08:32:14 -0600

    Fixed indentation error

--- redcap/project.py
+++ redcap/project.py
@@ -373,7 +373,7 @@ class Project(object):
         elif fields and self.def_field not in fields:
             new_fields = list(fields)
             if self.def_field not in fields:
-            new_fields.append(self.def_field)
+                new_fields.append(self.def_field)
         elif not fields:
             new_fields = self.field_names
         else:
