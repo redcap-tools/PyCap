e0e8a89ed7a4f10b49a56d7b8666ad0d282f7e6b
pwildenhain <35195136+pwildenhain@users.noreply.github.com>
2020-05-08 14:37:18 -0400

    :bug: Fix package versioning (#113)

--- redcap/version.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-__author__ = 'Scott Burns <scott.s.burns@gmail.com>'
-__license__ = 'MIT'
-__copyright__ = '2014, Vanderbilt University'
-
-VERSION = '1.0.2'
