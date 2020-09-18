7303cc0f26d41fdd0222046954a0e1207130634e
Chris Greer <greer@as.arizona.edu>
2020-09-18 08:02:29 -0600

    :name_badge: Add support for exportFieldNames call (#125)

--- redcap/project.py
+++ redcap/project.py
@@ -192,6 +192,46 @@ class Project(object):
 
             return self.read_csv(StringIO(response), **df_kwargs)
 
+    def export_field_names(self, field=None, format='json', df_kwargs=None):
+        """
+        Export the project's export field names
+
+        Parameters
+        ----------
+        fields : str
+            Limit exported field name to this field (only single field supported).
+            When not provided, all fields returned.
+        format : (``'json'``), ``'csv'``, ``'xml'``, ``'df'``
+            Return the metadata in native objects, csv or xml.
+            ``'df'`` will return a ``pandas.DataFrame``.
+        df_kwargs : dict
+            Passed to ``pandas.read_csv`` to control construction of
+            returned DataFrame.
+            by default ``{'index_col': 'original_field_name'}``
+
+        Returns
+        -------
+        metadata : list, str, ``pandas.DataFrame``
+            metadata structure for the project.
+        """
+        ret_format = format
+        if format == 'df':
+            ret_format = 'csv'
+
+        pl = self.__basepl('exportFieldNames', format=ret_format)
+
+        if field:
+            pl['field'] = field
+
+        response, _ = self._call_api(pl, 'exp_field_names')
+
+        if format in ('json', 'csv', 'xml'):
+            return response
+        elif format == 'df':
+            if not df_kwargs:
+                df_kwargs = {'index_col': 'original_field_name'}
+            return self.read_csv(StringIO(response), **df_kwargs)
+
     def export_metadata(self, fields=None, forms=None, format='json',
             df_kwargs=None):
         """
