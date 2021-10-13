a40e503cc547038c1d779a246855934f41cd0398
pwildenhain <35195136+pwildenhain@users.noreply.github.com>
2021-10-13 11:49:38 -0400

    Add POC integration test (#159)
    * :truck: Reorganize into unit/integration tests
    
    * :building_construction: Add POC integration test

---
+++ test/integration/test_simple_project.py
@@ -0,0 +1,19 @@
+"""Test suite for Project class against real REDCap server"""
+# pylint: disable=missing-function-docstring
+import os
+
+import pytest
+from redcap import Project
+
+LOCAL_DEV = os.getenv("LOCAL_DEV")
+
+
+@pytest.mark.skipif(
+    not LOCAL_DEV, reason="Need to migrate to GitHub Actions before testing on CI"
+)
+def test_export_of_simple_project():
+    url = "https://redcapdemo.vanderbilt.edu/api/"
+    token = os.getenv("REDCAPDEMO_SIMPLE_TOKEN")
+    simple_proj = Project(url, token)
+    proj_records_export = simple_proj.export_records()
+    assert len(proj_records_export) == 3
