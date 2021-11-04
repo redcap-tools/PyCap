262b8caa4a563bf53cbe6b6f92d58b8a5f40f2eb
Paul Wildenhain <wildenhaip@chop.edu>
2021-11-04 12:11:35 -0400

    :construction: Add more tests for simple project

--- test/unit/test_simple_project.py
+++ test/unit/test_simple_project.py
@@ -1,16 +1,147 @@
 #! /usr/bin/env python
 """Test suite for Project class against mocked REDCap server"""
 # pylint: disable=missing-function-docstring
-from redcap import Project
+from io import StringIO
+
+import pandas as pd
+import pytest
+
+from redcap import Project, RedcapError
 
 
-def test_basic_init(simple_project):
+def test_bad_creds(project_urls, project_token):
+    # this url is "bad" because we didn't set up an response fixture for it
+    bad_url = project_urls["bad_url"]
+
+    with pytest.raises(RedcapError):
+        Project(bad_url, project_token)
+    with pytest.raises(RedcapError):
+        Project(bad_url, "1")
+
+
+def test_init(simple_project):
     assert isinstance(simple_project, Project)
 
 
-def test_simple_json_export(simple_project):
+def test_attrs(simple_project):
+    for attr in (
+        "metadata",
+        "field_names",
+        "field_labels",
+        "forms",
+        "events",
+        "arm_names",
+        "arm_nums",
+        "def_field",
+    ):
+        assert hasattr(simple_project, attr)
+
+
+def test_is_not_longitudinal(simple_project):
+    assert not simple_project.is_longitudinal()
+
+
+def test_events_and_arms_attrs_are_empty(simple_project):
+    for attr in "events", "arm_names", "arm_nums":
+        attr_obj = getattr(simple_project, attr)
+        assert attr_obj == ()
+
+
+def test_import_records(simple_project):
+    data = simple_project.export_records()
+    response = simple_project.import_records(data)
+
+    assert "count" in response
+    assert not "error" in response
+
+
+def test_bad_import_throws_exception(simple_project):
+    data = simple_project.export_records()
+    data[0]["non_existent_key"] = "foo"
+
+    with pytest.raises(RedcapError) as assert_context:
+        simple_project.import_records(data)
+
+    assert "error" in repr(assert_context)
+
+
+def test_metadata_csv_export(simple_project):
+    metadata_csv_export = simple_project.export_metadata(format="csv")
+    data = pd.read_csv(StringIO(metadata_csv_export))
+
+    assert len(data) == 1
+
+
+def test_metadata_df_export(simple_project):
+    dataframe = simple_project.export_metadata(format="df")
+
+    assert isinstance(dataframe, pd.DataFrame)
+
+
+def test_metadata_export_passes_filters_as_arrays(simple_project, mocker):
+    mocked_api_call = mocker.patch.object(
+        simple_project, "_call_api", return_value=(None, None)
+    )
+
+    simple_project.export_metadata(
+        fields=["field0", "field1", "field2"],
+        forms=["form0", "form1", "form2"],
+    )
+
+    args, _ = mocked_api_call.call_args
+
+    payload = args[0]
+
+    assert payload["fields[0]"] == "field0"
+    assert payload["fields[1]"] == "field1"
+    assert payload["fields[2]"] == "field2"
+    assert payload["forms[2]"] == "form2"
+
+
+def test_metadata_import(simple_project):
+    data = simple_project.export_metadata()
+    response = simple_project.import_metadata(data)
+
+    assert response == len(data)
+
+
+def test_reduced_metadata_import(simple_project):
+    original_data = simple_project.export_metadata()
+    # reducing the metadata
+    reduced_data = original_data[0:1]
+    imported_data = simple_project.import_metadata(reduced_data)
+
+    assert len(reduced_data) == imported_data
+
+
+def test_json_export(simple_project):
     data = simple_project.export_records()
 
     assert isinstance(data, list)
+
     for record in data:
         assert isinstance(record, dict)
+
+
+def test_csv_export(simple_project):
+    csv_export = simple_project.export_records(format="csv")
+    data = pd.read_csv(StringIO(csv_export))
+
+    assert len(data) == 1
+
+
+def test_df_export(simple_project):
+    dataframe = simple_project.export_records(format="df")
+
+    assert isinstance(dataframe, pd.DataFrame)
+    # Test it's a normal index
+    assert hasattr(dataframe.index, "name")
+
+
+def test_df_export_correctly_uses_df_kwargs(simple_project):
+    dataframe = simple_project.export_records(
+        format="df", df_kwargs={"index_col": "first_name"}
+    )
+    assert dataframe.index.name == "first_name"
+    # the default index column is just a regular column
+    assert "study_id" in dataframe
