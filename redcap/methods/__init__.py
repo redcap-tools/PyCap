9109e0b1c627eadd00144036b39973c889782507
Paul Wildenhain <wildenhaip@chop.edu>
2021-12-03 21:39:32 -0500

    :heavy_plus_sign: Add Surveys class

---
+++ redcap/methods/__init__.py
@@ -0,0 +1,11 @@
+"""Make the method modules available upon import"""
+
+import redcap.methods.field_names
+import redcap.methods.files
+import redcap.methods.instruments
+import redcap.methods.metadata
+import redcap.methods.project_info
+import redcap.methods.records
+import redcap.methods.reports
+import redcap.methods.surveys
+import redcap.methods.users
