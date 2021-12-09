5f760d634156217170e6e80dc7a0102c332e8097
Paul Wildenhain <wildenhaip@chop.edu>
2021-12-09 15:03:16 -0500

    :building_construction: Add real long project fixture

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -3,38 +3,34 @@
 from datetime import datetime
 import os
 
+from pathlib import Path
+
 import pytest
 import requests
 
 from redcap import Project
 
-
-@pytest.fixture(scope="module")
-def redcapdemo_url():
-    """API url for redcapdemo testing site"""
-    return "https://redcapdemo.vanderbilt.edu/api/"
+SUPER_TOKEN = os.getenv("REDCAPDEMO_SUPERUSER_TOKEN")
 
 
-@pytest.fixture(scope="module")
-def simple_project_token(redcapdemo_url):
-    """Create a simple project for testing on redcapdemo.vanderbilt.edu
+def create_project(url: str, super_token: str, project_xml_path: Path) -> str:
+    """Create a project for testing on redcapdemo.vanderbilt.edu
     This API method returns the token for the newly created project, which
-    is passed to the simple project tests
+    used for the integration tests
     """
-    super_token = os.getenv("REDCAPDEMO_SUPERUSER_TOKEN")
     current_time = datetime.now().strftime("%m-%d %H:%M:%S")
-    project_title = f"PyCap Test Simple Project: {current_time}"
+    project_title = f"PyCap { project_xml_path.stem }: { current_time }"
     project_info = f"""<?xml version="1.0" encoding="UTF-8" ?>
     <item>
     <project_title>{project_title}</project_title>
     <purpose>0</purpose>
     </item>"""
 
-    with open("tests/data/test_simple_project.xml", encoding="UTF-8") as proj_xml_file:
+    with open(project_xml_path, encoding="UTF-8") as proj_xml_file:
         project_data = proj_xml_file.read()
 
     res = requests.post(
-        url=redcapdemo_url,
+        url=url,
         data={
             "token": super_token,
             "content": "project",
@@ -48,6 +44,36 @@ def simple_project_token(redcapdemo_url):
 
 
 @pytest.fixture(scope="module")
+def redcapdemo_url() -> str:
+    """API url for redcapdemo testing site"""
+    return "https://redcapdemo.vanderbilt.edu/api/"
+
+
+@pytest.fixture(scope="module")
+def simple_project_token(redcapdemo_url) -> str:
+    """Create a simple project and return it's API token"""
+    simple_project_xml_path = Path("tests/data/test_simple_project.xml")
+    project_token = create_project(redcapdemo_url, SUPER_TOKEN, simple_project_xml_path)
+
+    return project_token
+
+
+@pytest.fixture(scope="module")
 def simple_project(redcapdemo_url, simple_project_token):
     """A simple REDCap project"""
     return Project(redcapdemo_url, simple_project_token)
+
+
+@pytest.fixture(scope="module")
+def long_project_token(redcapdemo_url) -> str:
+    """Create a long project and return it's API token"""
+    long_project_xml_path = Path("tests/data/test_long_project.xml")
+    project_token = create_project(redcapdemo_url, SUPER_TOKEN, long_project_xml_path)
+
+    return project_token
+
+
+@pytest.fixture(scope="module")
+def long_project(redcapdemo_url, long_project_token):
+    """A long REDCap project"""
+    return Project(redcapdemo_url, long_project_token)
