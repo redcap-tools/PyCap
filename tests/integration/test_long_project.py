4e38e7dabf89eacb06563c9cb7e4426ecccaed7a
Paul Wildenhain <wildenhaip@chop.edu>
2022-01-22 15:42:09 -0500

    :white_check_mark: Add new tests for new df returns
    Also needed to modify the test long project to enable one of the instruments as a survey

--- tests/integration/test_long_project.py
+++ tests/integration/test_long_project.py
@@ -15,3 +15,25 @@ if not os.getenv("REDCAPDEMO_SUPERUSER_TOKEN"):
 @pytest.mark.integration
 def test_is_longitudinal(long_project):
     assert long_project.is_longitudinal
+
+
+def test_survey_participant_export(long_project):
+    data = long_project.export_survey_participant_list(
+        instrument="contact_info", event="enrollment_arm_1"
+    )
+    assert len(data) == 1
+
+    data = long_project.export_survey_participant_list(
+        instrument="contact_info", format_type="df", event="enrollment_arm_1"
+    )
+    assert "email" in data.columns
+
+
+def test_project_info_export(long_project):
+    data = long_project.export_project_info()
+    assert data["purpose"] == 0
+
+
+def test_users_export(long_project):
+    data = long_project.export_users(format_type="df", df_kwargs={"index_col": "email"})
+    assert data.index.name == "email"
