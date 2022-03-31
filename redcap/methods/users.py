e6fc579fc06a574e20f620c745b05339abf7fad8
Paul Wildenhain <wildenhaip@chop.edu>
2022-03-31 15:42:32 -0400

    :sparkles: Add import_user method

--- redcap/methods/users.py
+++ redcap/methods/users.py
@@ -1,5 +1,5 @@
 """REDCap API methods for Project users"""
-from typing import TYPE_CHECKING, Any, Dict, Literal, Optional, overload
+from typing import TYPE_CHECKING, Any, Dict, List, Literal, Optional, Union, overload
 
 from redcap.methods.base import Base, Json
 
@@ -74,3 +74,80 @@ class Users(Base):
             format_type=format_type,
             df_kwargs=df_kwargs,
         )
+
+    @overload
+    def import_users(
+        self,
+        to_import: Union[str, List[Dict[str, Any]], "pd.DataFrame"],
+        return_format_type: Literal["json"],
+        import_format: Literal["json", "csv", "xml", "df"] = "json",
+    ) -> int:
+        ...
+
+    @overload
+    def import_users(
+        self,
+        to_import: Union[str, List[Dict[str, Any]], "pd.DataFrame"],
+        return_format_type: Literal["csv", "xml"],
+        import_format: Literal["json", "csv", "xml", "df"] = "json",
+    ) -> str:
+        ...
+
+    def import_users(
+        self,
+        to_import: Union[str, List[Dict[str, Any]], "pd.DataFrame"],
+        return_format_type: Literal["json", "csv", "xml"] = "json",
+        import_format: Literal["json", "csv", "xml", "df"] = "json",
+    ):
+        """
+        Import users/user rights into the REDCap Project
+
+        Args:
+            to_import: array of dicts, csv/xml string, `pandas.DataFrame`
+                Note:
+                    If you pass a csv or xml string, you should use the
+                    `import format` parameter appropriately.
+            return_format_type:
+                Response format. By default, response will be json-decoded.
+            import_format:
+                Format of incoming data. By default, to_import will be json-encoded
+
+        Returns:
+            Union[int, str]: Number of users added or updated
+
+        Examples:
+            Add test user. Only username is required
+            >>> test_user = [{"username": "pandeharris@gmail.com"}]
+            >>> proj.import_users(test_user)
+            1
+
+            All currently valid options for user rights
+            >>> test_user = [
+            ...     {"username": "pandeharris@gmail.com", "email": "pandeharris@gmail.com",
+            ...     "firstname": "REDCap Trial", "lastname": "User", "expiration": "",
+            ...     "data_access_group": "", "data_access_group_id": "", "design": 0,
+            ...     "user_rights": 0, "data_export": 2, "reports": 1, "stats_and_charts": 1,
+            ...     "manage_survey_participants": 1, "calendar": 1, "data_access_groups": 0,
+            ...     "data_import_tool": 0, "data_comparison_tool": 0, "logging": 0,
+            ...     "file_repository": 1, "data_quality_create": 0, "data_quality_execute": 0,
+            ...     "api_export": 0, "api_import": 0, "mobile_app": 0,
+            ...     "mobile_app_download_data": 0, "record_create": 1, "record_rename": 0,
+            ...     "record_delete": 0, "lock_records_all_forms": 0, "lock_records": 0,
+            ...      "lock_records_customization": 0, "forms": {"form_1": 3}}
+            ... ]
+            >>> proj.import_users(test_user)
+            1
+        """
+        payload = self._initialize_import_payload(
+            to_import=to_import,
+            import_format=import_format,
+            return_format_type=return_format_type,
+            content="user",
+        )
+
+        return_type = self._lookup_return_type(
+            format_type=return_format_type, request_type="import"
+        )
+        response = self._call_api(payload, return_type)
+
+        return response
