3a8a117436f2f37d2bafd9685f9f3bc33e5134f7
Paul Wildenhain <wildenhaip@chop.edu>
2022-08-03 12:26:37 -0400

    :memo: Update docstring to reflect API changes

--- redcap/methods/user_roles.py
+++ redcap/methods/user_roles.py
@@ -48,14 +48,14 @@ class UserRoles(Base):
         Examples:
             >>> proj.export_user_roles()
             [{'unique_role_name': ..., 'role_label': 'Test role', 'design': '0', 'user_rights': '0',
-            'data_access_groups': '0', 'data_export': '0', 'reports': '0', 'stats_and_charts': '0',
+            'data_access_groups': '0', 'reports': '0', 'stats_and_charts': '0',
             'manage_survey_participants': '0', 'calendar': '0', 'data_import_tool': '0',
             'data_comparison_tool': '0', 'logging': '0', 'file_repository': '0',
             'data_quality_create': '0', 'data_quality_execute': '0', 'api_export': '0',
             'api_import': '0', 'mobile_app': '0', 'mobile_app_download_data': '0',
             'record_create': '0', 'record_rename': '0', 'record_delete': '0',
-            'lock_records_customization': '0', 'lock_records': '0', 'lock_records_all_forms': '0',
-            'forms': {'form_1': 2}}]
+            'lock_records_customization': '0', 'lock_records': '0', ...,
+            'forms': {'form_1': 2}, 'forms_export': {'form_1': 0}}]
         """
         payload = self._initialize_payload(content="userRole", format_type=format_type)
         return_type = self._lookup_return_type(format_type, request_type="export")
